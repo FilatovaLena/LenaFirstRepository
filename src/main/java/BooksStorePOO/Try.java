0730e5a72a1a02b906537db67267a6dfc2b87a53
FilatovaLena <139183810+FilatovaLena@users.noreply.github.com>
2023-07-17 00:03:48 -0400

    practice for pushing code to master branches 2

--- src/main/java/BooksStorePOO/Try.java
+++ src/main/java/BooksStorePOO/Try.java
@@ -4,5 +4,6 @@ public class Try {
     public static void main(String[] args) {
         System.out.println("Elena Filatova");
         System.out.println("Fillatova");
+        System.out.println("Fillatova");
     }
 }
